345fb85e796c18401f1f9dd11246dddd39853c84
akamaryna <marina.akatieva@gmail.com>
2025-02-10 20:58:18 +0100

    feat: scaffold vue app

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <header>
+
+  </header>
+  
+  <main>
+    <HelloWorld msg="You did it!" />
+  </main>
+</template>
+
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
